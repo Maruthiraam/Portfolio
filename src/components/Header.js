45b0ed93074d1847b4b5e1db91b6c80805e94228
Maruthiraam <hareshriraam@gmail.com>
2025-07-17 16:17:58 +0530

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,13 @@ import { Bars3Icon } from '@heroicons/react/24/solid'
 export default function Header()
 {   const[ToggleMenu,setToggleMenu]=useState(false);
     return <header className="flex justify-between px-5 py-2 bg-primary">
-        <a className="font-bold text-black"href="#">Ram Vibe</a>
+        <a className="font-bold text-white "href="#">ROBO</a>
         <nav className="hidden md:block">
         <ul className="flex  text-white">
-            <li><a href="#">HOME</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Projects</a></li>
-            <li><a href="#">Connect with</a></li>
+            <li><a href="#"className=' hidden md:block hover:text-black'>HOME</a></li>
+            <li><a href="#"className='hover:text-black'>About</a></li>
+            <li><a href="#"className='hover:text-black'>Projects</a></li>
+            <li><a href="#"className='hover:text-black'>Connect with</a></li>
         </ul>
         </nav>
         {ToggleMenu &&<nav className="block md:hidden ">
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,22 @@
-import neon from 'src/assets/1.avif';
-
+import man from '../asserts/baymax.png';
+import { FaLinkedin } from "react-icons/fa";
+  
 export default function Hero() {
   return (
-    <section>
-      <h1>👋🏽 Hare Shri Raam.N</h1>
-      <p>I am currently studying BTech 2nd Year. I am passionate about Robotics and cool Tech stuff.</p>
-      <img src={neon} alt="Profile banner" />
+    <section className="flex flex-col md:flex px-5 py-32 bg-secondary justify-center" >
+      <div className="w-1/2 flex flex-col"><h1 className=" text-white text-6xl font-venom-font">🤙🏼Hi,<br/>
+                I am <span className="text-black ">Hare</span> shri <span className="text-black ">Raam</span>.N
+        <p className="text-2xl">
+        I am currently studying BTech 2nd Year.<br/> I am passionate about Robotics and cool Tech stuff👾.
+      </p>
+      </h1>
+      <div classNmae="flex py-15 pr-5">
+        <a href='#'className='pr-5 hover:text-white'><FaLinkedin size={40}/></a>
+      </div>
+      </div>
+      <div className="flex w-1/2 justify-center">
+        <img src={man} className="md:w-1/3"/>
+      </div>
     </section>
   );
 }
